13748590d55e8353fa426908c2efc323a5785c59
Tampy <tpmx13@gmail.com>
2015-09-07 16:27:02 -0400

    Add Google Test functionality.

--- game.m.cpp
+++ game.m.cpp
@@ -1,10 +1,22 @@
 // game.m.cpp
 
 #include "src/game.h"
+#include <cmath>
 #include <iostream>
 
-using namespace StevensDev;
+// Random unit test function A.
+double square(double root) {
+	return root * root;
+}
+
+// Random unit test function B.
+double squareRoot(double value) {
+	return sqrt(value);
+}
 
 int main(int argc, char *argv[]) {
+	using namespace StevensDev;
+	std::cout << square(9.0) << std::endl;
 	std::cout << "Howdy." << std::endl;
+	return 0;
 }
--- src/game.h
+++ src/game.h
@@ -1,9 +1,23 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <cmath>
+
 namespace StevensDev
 {
-	int main();
+	
 }
+
+class Testing {
+	public:
+		static double square(double root) {
+			return root * root;
+		}
+		static double squareRoot(double value) {
+			return sqrt(value);
+		}
+};
+
+int main(int, char**);
 
 #endif
